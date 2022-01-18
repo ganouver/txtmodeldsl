8654f91950cf27afbc4a5bf5ede47b46f6dbfa99
Boris Romanov <romanov.boris@alphaopen.com>
2022-01-18 12:39:14 +0300

    add user stories

--- modelcore/src/main/kotlin/org/model/core/Model.kt
+++ modelcore/src/main/kotlin/org/model/core/Model.kt
@@ -1,7 +1,13 @@
 package org.model.core
 
+import org.model.sm.Role
+
 open class Model(ID : String, label : String) : Package(root, ID, label, "")
 {
+    protected fun story(role: Role, action: String, goal: String) {
+        TODO("Not yet implemented")
+    }
+
     fun validate() {
         assert(true)
     }
--- testmodel/src/main/kotlin/ao/mobile/market/marketplace.kt
+++ testmodel/src/main/kotlin/ao/mobile/market/marketplace.kt
@@ -3,14 +3,19 @@ package ao.mobile.market
 
 import org.model.*
 import org.model.core.*
+import org.model.sm.Role
 import org.model.sm.System
 
-class MobileAppSystem(parent : Model, ID : String) : System(parent, ID, "Мобильное приложение", false,"" ) {
+class MobileAppSystem(parent : Model, ID : String)
+      : System(parent, ID, "Мобильное приложение", false,"" ) {
    val mapp = container("app","Mobile application","iOS/Android"   )
+
+   val be   = container("be","Backend",".NET Core"   )
 
 }
 
-class MarketModel : Model("marketplace", "Маркетплейс") {
+class MarketModel
+      : Model("marketplace", "Маркетплейс") {
 
     val worker = role("worker", "Работник УК")
     val resident = role("Resident", "Житель ")
@@ -22,14 +27,17 @@ class MarketModel : Model("marketplace", "Маркетплейс") {
     val mapps = MobileAppSystem(this, "mapp_system")
 
     init {
+        //user stories
+        story(worker, "Настраивает каталог товаров", "информировать потребителей о доступных товарах")
         worker
             .need("inform_residents", "донести до жителей информацию о доступных услугах/товарах", "чтобы жители могли воспользоваться услугами и оплатить их")
 
         resident
             .inherit(customer)
 
+        story(customer, "Изучает содержимое каталога товаров", "узнать о предлагаемых УК услугах/товарах и принять решение об их приобретении")
         customer
-            .need("know", "узнать о предлагаемых УК услугах/товарах", "чтобы принять решение об их приобретении")
+            .need("know", "")
             .need("find and order", " найти и заказать нужную услугу/товар ", " чтобы воспользоваться предлагаемой услугой")
             .act(
                 usecase("view_catalog", "Просмотр каталога услуг")
@@ -42,5 +50,5 @@ class MarketModel : Model("marketplace", "Маркетплейс") {
         mapps.accessTo(bitrix, "Использует API маркетплейса")
     }
 
-}
 
+}
